e575a91e3c37347ff2c4f60703fedbbdf4159b27
Administrátor <acq@acq.sk>
2022-02-25 13:15:32 +0100

    New package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nodsec:two-factor',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Two-factor authentication for accounts-password forked from enhanced with verification method',
   git: 'https://github.com/nodsec/meteor-two-factor.git',
   documentation: 'README.md'
